bc1a40370eae8af8e8de9a2d194917bd484ddb57
travis88 <hellsballs99@gmail.com>
2016-06-25 19:38:00 +0300

    translit

--- 5/action.php
+++ 5/action.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <title>Информация про город</title>
+    <style>
+        * {
+            font-family: Ubuntu, sans-serif;
+        }
+    </style>
 </head>
 <?php
 $city = $_GET['city'];
+echo $city."<br>";
 include 'simple_html_dom.php';
 $html = new simple_html_dom();
 //Население
@@ -36,18 +42,48 @@ foreach ($collection as $item) {
 }
 echo "<br>";
 //Погода
-$request2 = "http://openweathermap.org/find?q=".$city;
-//echo $request2;
-//$html2 = new simple_html_dom();
+//Используем функцию перевода в транслит для городов написанных на кириллице 
+$citytranslit = rus2translit($city);
+$request2 = "https://pogoda.yandex.ru/".$citytranslit;
 $html->load_file($request2);
-//echo $html;
-$collection2 = $html->find("table[class=table]");
+$collection2 = $html->find("div[class=current-weather__thermometer_type_now]");
 foreach ($collection2 as $item2) {
     $elements2 = strip_tags($item2);
     $element2 = explode(" ", $elements2);
     foreach ($element2 as $value2) {
-        echo $value2;
+        echo "Погода ".$value2."<br>";
     }
+}
+//echo $citytranslit;
+
+
+function rus2translit($string) {
+    $converter = array(
+        'а' => 'a',   'б' => 'b',   'в' => 'v',
+        'г' => 'g',   'д' => 'd',   'е' => 'e',
+        'ё' => 'e',   'ж' => 'zh',  'з' => 'z',
+        'и' => 'i',   'й' => 'y',   'к' => 'k',
+        'л' => 'l',   'м' => 'm',   'н' => 'n',
+        'о' => 'o',   'п' => 'p',   'р' => 'r',
+        'с' => 's',   'т' => 't',   'у' => 'u',
+        'ф' => 'f',   'х' => 'h',   'ц' => 'c',
+        'ч' => 'ch',  'ш' => 'sh',  'щ' => 'sch',
+        'ь' => '\'',  'ы' => 'y',   'ъ' => '\'',
+        'э' => 'e',   'ю' => 'yu',  'я' => 'ya',
+
+        'А' => 'A',   'Б' => 'B',   'В' => 'V',
+        'Г' => 'G',   'Д' => 'D',   'Е' => 'E',
+        'Ё' => 'E',   'Ж' => 'Zh',  'З' => 'Z',
+        'И' => 'I',   'Й' => 'Y',   'К' => 'K',
+        'Л' => 'L',   'М' => 'M',   'Н' => 'N',
+        'О' => 'O',   'П' => 'P',   'Р' => 'R',
+        'С' => 'S',   'Т' => 'T',   'У' => 'U',
+        'Ф' => 'F',   'Х' => 'H',   'Ц' => 'C',
+        'Ч' => 'Ch',  'Ш' => 'Sh',  'Щ' => 'Sch',
+        'Ь' => '\'',  'Ы' => 'Y',   'Ъ' => '\'',
+        'Э' => 'E',   'Ю' => 'Yu',  'Я' => 'Ya',
+    );
+    return strtr($string, $converter);
 }
 
 ?>
